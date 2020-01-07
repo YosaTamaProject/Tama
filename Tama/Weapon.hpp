b0a669125a4bdd3ffebe232ccc99a56cf8b2025f
Ito Haruto <s1701015@sendai-nct.jp>
2020-01-07 16:45:18 +0900

    :heavy_plus_sign: weaponBaseクラスの追加

--- Tama/Weapon.hpp
+++ Tama/Weapon.hpp
@@ -4,6 +4,24 @@
 #ifndef WEAPON_HPP
 #define WEAPON_HPP
 
+#include "Bullet.hpp"
+
 // ここに武器の基底クラスを記述する
+class WeaponBase
+{
+public:
+	WeaponBase()
+	{
+
+	}
+
+	virtual void update() = 0;
+	virtual void draw() const = 0;
+	virtual Array<Bullet> getBullets() = 0;
+
+private:
+	virtual void shot() = 0;
+
+};
 
 #endif
