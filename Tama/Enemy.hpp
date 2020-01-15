57318ff7a3ba103d442ba570462f606343336995
Ito Haruto <s1701015@sendai-nct.jp>
2020-01-15 22:30:37 +0900

    :tada: Enemyクラスの仮実装 中身はUserクラスとほとんど変わらないです。

--- Tama/Enemy.hpp
+++ Tama/Enemy.hpp
@@ -5,5 +5,33 @@
 #define ENEMY_HPP
 
 // ここにパーソンから派生した敵のクラスを記述する
+class Enemy : public PersonBase
+{
+public:
+	Enemy() : Enemy(nullptr, 0, Vec2(0, 0))
+	{
+	}
+
+	Enemy(WeaponBase* wp, const int hp, const Vec2 pos) : PersonBase(wp, hp, pos)
+	{
+	}
+
+	// デストラクタ(未実装)
+	~Enemy()
+	{
+	}
+
+	void draw() const override final
+	{
+		/*Print(this->get_hp());
+		Print(this->get_weapon());
+		Print(this->get_x());
+		Print(this->get_y());*/
+	}
+
+private:
+
+};
+
 
 #endif
