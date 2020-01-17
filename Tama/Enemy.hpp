cd67c1b1574fecd5f7ee84e9c58dcd0208dcb1a6
Ito Haruto <s1701015@sendai-nct.jp>
2020-01-17 09:30:25 +0900

    :recycle: Enemyクラスの内容を変更

--- Tama/Enemy.hpp
+++ Tama/Enemy.hpp
@@ -8,11 +8,17 @@
 class Enemy : public PersonBase
 {
 public:
-	Enemy() : Enemy(nullptr, 0, Vec2(0, 0))
+	Enemy() : Enemy(nullptr, Texture(), 0, 0, 0, Vec2(0, 0))
 	{
 	}
 
-	Enemy(WeaponBase* wp, const int hp, const Vec2 pos) : PersonBase(wp, hp, pos)
+
+	Enemy(WeaponBase* wp, Texture image, const int width, const int height, const int hp, const Vec2 pos) : PersonBase(wp, hp, pos), image_(image.resized(width, height)), collision_(Rect(pos.asPoint(), width, height))
+	{
+	}
+	
+
+	Enemy(WeaponBase* wp, TextureRegion resized_image, const int hp, const Vec2 pos) : PersonBase(wp, hp, pos), image_(resized_image), collision_(Rect(pos.asPoint(), resized_image.size.x, resized_image.size.y))
 	{
 	}
 
@@ -21,16 +27,25 @@ public:
 	{
 	}
 
+
+	void update()
+	{
+		
+	}
+	
+
 	void draw() const override final
 	{
 		/*Print(this->get_hp());
 		Print(this->get_weapon());
 		Print(this->get_x());
 		Print(this->get_y());*/
+		image_.drawAt(get_pos());
 	}
 
 private:
-
+	TextureRegion image_;
+	Rect collision_;
 };
 
 
